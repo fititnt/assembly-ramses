16975ce151f3d2d3a3c689be9eea693fd9ade232
Emerson Rocha Luiz <emerson@alligo.com.br>
2014-09-05 17:33:46 +0000

    Adicionado testes no exemplo em C

--- main.c
+++ main.c
@@ -15,15 +15,27 @@
 char TOTAL;
 
 // Testes
-char LUCRO[] = { 5, 7, 9, -1, -2, -3};
-unsigned char VISITAS[] = { 4, 2, 4, 0, 4};
+// 0. Resultado 10
+//char LUCRO[] = { 5, 7, 9, -1, -2, -3};
+//unsigned char VISITAS[] = { 4, 2, 4, 0, 4};
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+// 1. Resultado 10
+//char LUCRO[] = {5, 7, 9, -1, -2, -3};
+//unsigned char VISITAS[] = {4, 2, 4, 0, 4};
 
-  // Usar registradores
+// 2. Resultado 172
+char LUCRO[] = {1,-1,2,3,5,7,-11,13,17,19,-23,29,31,-3,-1,-2};
+unsigned char VISITAS[] = {30,0,2,4,6,8,10,12,14,1,3,5,7,9,11,13,15,15,13,12,11,10,1,2,3,4,5,6,7,8,9};
+
+// 3. Resultado 0
+//char LUCRO[] = {1, 1, 2, 2, 3, 3};
+//unsigned char VISITAS[] = {0, 1, 1, 1};
+
+// 4. Resultado 52
+//char LUCRO[] = {1,2,3,4,5,6,7,-1,-2,-3,-4,-5,-6,-7,11,53};
+//unsigned char VISITAS[] = {13,0,1,2,3,4,8,9,10,11,15,1,7,9,5};
+
+int calculo() {
   int i = 1, soma = 0;
 
   while (i <= VISITAS[0]) {
@@ -31,9 +43,14 @@ int main(int argc, char** argv) {
     soma += LUCRO[VISITAS[i]];
     i++;
   }
+  return soma;
+}
 
-  // Atualizar memória apenas no final
-  TOTAL = soma;
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  TOTAL = calculo();
 
   printf("Total: %d\n", TOTAL);
   return (0);
