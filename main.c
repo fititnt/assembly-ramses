e1ba249c31706a4005050fe382dedf40506eea88
Emerson Rocha Luiz <emerson@alligo.com.br>
2014-09-05 15:37:19 +0000

    Iniciado

---
+++ main.c
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.c
+ * Author: fititnt
+ *
+ * Created on 5 de Setembro de 2014, 15:25
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char LUCRO[16];
+unsigned char VISITAS[31];
+char TOTAL;
+
+//char LUCRO[] = { 5, 7, 9, -1, -2, -3};
+//unsigned char VISITAS[] = { 4, 2, 4, 0, 4};
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  printf("Hello World!\n");
+  return (EXIT_SUCCESS);
+}
+
