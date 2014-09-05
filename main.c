2623cb0b5dd8fb3dcea1067e19d63bbe83b42135
Emerson Rocha Luiz <emerson@alligo.com.br>
2014-09-05 16:14:01 +0000

    Primeiro teste parcial em C puro

--- main.c
+++ main.c
@@ -8,19 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char LUCRO[16];
-unsigned char VISITAS[31];
+//char LUCRO[16];
+//unsigned char VISITAS[31];
 char TOTAL;
 
 //char LUCRO[] = { 5, 7, 9, -1, -2, -3};
 //unsigned char VISITAS[] = { 4, 2, 4, 0, 4};
+int LUCRO[] = { 5, 7, 9, -1, -2, -3};
+int VISITAS[] = { 4, 2, 4, 0, 4};
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+  int i = 1, soma = 0;
+  while (i <= VISITAS[0]) {
+    printf("Visita: %d Cidade: %d Lucro Atual: %d\n", i, VISITAS[i], LUCRO[VISITAS[i]]);
+    soma += LUCRO[VISITAS[i]];
+    i++;
+  }
 
-  printf("Hello World!\n");
+  printf("Total: %d\n", soma);
   return (EXIT_SUCCESS);
 }
 
